cd4766a28912c34e5fc62340539a2f3784f1ba2e
sandpinedo <1440690@senati.pe>
2023-08-17 11:22:02 -0500

    ingrese el comando que solicita la accion para seleccionar una operacion y las opciones de la operacion que se quiere realizar

--- Sumayresta.java
+++ Sumayresta.java
@@ -15,17 +15,19 @@ public class Sumayresta {
     public static void main(String[] args) {
        Scanner scanner = new Scanner (System.in);
        
-    System.out.print("Ingrese el primer número: ");
-      double num1 = scanner.nextDouble();
+      System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
 
         System.out.print("Ingrese el segundo número: ");
         double num2 = scanner.nextDouble();
 
-        
+        System.out.println("Seleccione la operación:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
 
         
 
         
 
-    }   
+        
     }
