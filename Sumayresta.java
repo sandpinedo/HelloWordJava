822a791c71b06aec150938b8b0f9f389131c6871
sandpinedo <1440690@senati.pe>
2023-08-17 11:14:37 -0500

    puse el comando que pide el ingreso al primer y segundo numero para realizar la operacion

--- Sumayresta.java
+++ Sumayresta.java
@@ -15,5 +15,17 @@ public class Sumayresta {
     public static void main(String[] args) {
        Scanner scanner = new Scanner (System.in);
        
-   
+    System.out.print("Ingrese el primer número: ");
+      double num1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        
+
+        
+
+        
+
+    }   
     }
